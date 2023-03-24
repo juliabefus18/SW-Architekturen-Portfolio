2ecdc17ad6d94e92860f249730dfad1dbfbd1da6
Giuseppe <85261127+GiuseppeF96@users.noreply.github.com>
2023-03-24 15:00:28 +0100

    Added dependencies

--- Portfolio/src/Main.java
+++ Portfolio/src/Main.java
@@ -1,5 +1,10 @@
+import de.dhbw.ka.se2.vecto4j.input.VehicleInput;
+import com.fasterxml.jackson.core.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VehicleInput vi = new VehicleInput();
+        vi.setWeight(1000);
+        System.out.println("weight = " + vi.getWeight());
     }
 }
